76421bd52fa03b5cf013c6578a2babe9967db978
Darwin Schweitzer <darwinteals@outlook.com>
2020-10-16 15:23:59 +0000

    roadTrip

--- studentWork/Chapter3/Equestria.java
+++ studentWork/Chapter3/Equestria.java
@@ -5,13 +5,13 @@ public class Equestria {
     public static void main(String[] args) {
         double diameter = 2.0;
         double circumference = 0;
-        roadTrip(diameter);
-        System.out.println(circumference);
+        circumference = roadTrip(diameter);
+        System.out.println("Returned Circumference: " + circumference);
     }
 
     public static double roadTrip(double diameter) {
         double circumference = diameter * Math.PI;
-        System.out.println("In Method" + circumference);
+        System.out.println("In roadTrip method Circumference: " + circumference);
         return circumference;
 
     }
