b6e9a8a9b5e882223fb4a985ddab0cee6bab233f
darsch <darwinschweitzer@hotmail.com>
2020-11-24 21:06:45 +0000

    Modified CodespacesTest.java

--- studentWork/GitHubCodespacesTest/CodespacesBeta.java
+++ studentWork/GitHubCodespacesTest/CodespacesBeta.java
@@ -2,7 +2,7 @@ package studentWork.GitHubCodespacesTest;
 
 public class CodespacesBeta {
     public static void main(String[] args) {
-        String x = "Merry Christmas !!!";
+        String x = "Happy New Year !!!";
         System.out.println(x);
         
     }
